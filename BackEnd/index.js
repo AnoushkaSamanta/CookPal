d51389168f975a72ed051b719534d5482a3e2fc8
AnoushkaSamanta <anoushka1701@gmail.com>
2025-01-09 23:32:23 +0530

    Bcrypt

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,19 +1,30 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const cors=require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const UserModel=require("./models/User")
 
 
 const app=express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/cookpal");
 
 app.post("/register",(req,res)=>{
-   UserModel.create(req.body)
-   .then(users=>res.json(users))
-   .catch(err=>res.json(err))
+    let {name,email,password}=req.body;
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt, (err,hash)=>{
+            UserModel.create({
+                name,email,password:hash
+            })
+            .then(users=>res.json(users))
+            .catch(err=>res.json(err))
+        })
+    })
+
 })
 
 app.post("/login",(req,res)=>{
@@ -21,12 +32,14 @@ app.post("/login",(req,res)=>{
     UserModel.findOne({email:email})
     .then(user=>{
         if(user){
-            if(user.password===password){
-                res.json("Success")
-            }
-            else{
-                res.json("Incorrect Email or Password")
-            }
+            bcrypt.compare(password, user.password, function (err, result){
+                if(result)
+                {res.json("Success")}
+                else{
+                    res.json("Incorrect Email or Password")
+                }
+            })
+           
         }
         else{
             res.json("User doesn't exist")
